a0ae4aa01c6ec1a869410ff234fab02fb3677a12
Niek Sanders <niek.sanders@gmail.com>
2022-03-13 11:41:01 +0100

    Stubs for traversal tests.

---
+++ graph/test_traverse.py
@@ -0,0 +1,28 @@
+"""
+Unittests for traverse module.
+"""
+import unittest
+
+
+class TraverseTests(unittest.TestCase):
+    """
+    Unittests for traverse module.
+    """
+
+    def test_dfs(self):
+        self.assertTrue(False)
+
+    def test_dfs_empty(self):
+        self.assertTrue(False)
+
+    def test_dfs_cyclic(self):
+        self.assertTrue(False)
+
+    def test_bfs(self):
+        self.assertTrue(False)
+
+    def test_bfs_empty(self):
+        self.assertTrue(False)
+
+    def test_bfs_cyclic(self):
+        self.assertTrue(False)
